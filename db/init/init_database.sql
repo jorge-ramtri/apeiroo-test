0f07870261af38781bcb2485ddee10e4b3d87514
jorge-ramtri <jorge.ramtri@gmail.com>
2025-04-02 14:38:27 +0200

    add test data

--- db/init/init_database.sql
+++
@@ -1,4 +0,0 @@
--- Uncomment while testing in local, for creating a fresh install.
-DROP DATABASE IF EXISTS to_do_list;
--- Create the ddbb for to_do_list, further updates and table creation should be done in a new sql file.
-CREATE DATABASE to_do_list IF NOT EXISTS to_do_list;
