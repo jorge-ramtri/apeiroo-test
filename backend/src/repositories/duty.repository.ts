bf834a6f25903c79533a6d7b643d1e052913d03d
jorge-ramtri <jorge.ramtri@gmail.com>
2025-04-05 01:29:17 +0200

    clean up

--- backend/src/repositories/duty.repository.ts
+++ backend/src/repositories/duty.repository.ts
@@ -16,7 +16,6 @@ export const insertDuty = async (duty: Duty): Promise<Duty> => {
 };
 
 export const getAllDuties = async (): Promise<Duty[]> => {
-  logger.info(`Entra en backend/src/repositories/duty.repository.getAllDuties`);
   const query = 'SELECT * FROM duties';
   try {
     const result = await pool.query(query);
@@ -27,20 +26,7 @@ export const getAllDuties = async (): Promise<Duty[]> => {
   }
 };
 
-// export const getDutyById = async (id: string): Promise<Duty | null> => {
-//   logger.info(`Entra en backend/src/repositories/duty.repository.getDutyById`);
-//   const query = 'SELECT * FROM duties WHERE id = $1';
-//   try {
-//     const result = await pool.query(query, [id]);
-//     return result.rows[0] || null;
-//   } catch (error: any) {
-//     logger.error(error.message)
-//     throw new DatabaseError('Failed to retrieve the task');
-//   }
-// };
-
 export const updateDuty = async (duty: Duty): Promise<boolean> => {
-  logger.info(`Entra en backend/src/repositories/duty.repository.updateDuty with duty:`);
   const query = 'UPDATE duties SET name = $1, completed = $2 WHERE id = $3';
   const result = await pool.query(query, [duty.name, duty.completed, duty.id]);
   if (result.rowCount === 0) {
@@ -51,7 +37,6 @@ export const updateDuty = async (duty: Duty): Promise<boolean> => {
 };
 
 export const deleteDutyById = async (id: string): Promise<boolean> => {
-  logger.info(`Entra en backend/src/repositories/duty.repository.deleteDutyById`);
   const query = 'DELETE FROM duties WHERE id = $1';
   const result = await pool.query(query, [id]);
   if (result.rowCount === 0) {
